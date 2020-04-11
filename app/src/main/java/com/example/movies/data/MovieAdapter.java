471bc0c102b98420430237b77434599f5c987ede
Kholkin <kholkins@mail.ru>
2020-04-11 16:09:39 +0300

    model Movie

--- app/src/main/java/com/example/movies/data/MovieAdapter.java
+++ app/src/main/java/com/example/movies/data/MovieAdapter.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.movies.R;
 import com.example.movies.model.Movie;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -31,11 +32,20 @@ public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.MovieViewHo
 
         View view = LayoutInflater.from(context).inflate(R.layout.movie_item, parent,false);
 
-        return null;
+        return new MovieViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie currentMovie = movies.get(position);
+
+        String title = currentMovie.getTitle();
+        String year = currentMovie.getYear();
+        String poster = currentMovie.getPosterURL();
+
+        holder.titleTextView.setText(title);
+        holder.yearTextView.setText(year);
+        Picasso.get().load(poster).fit().centerInside().into(holder.posterImageView);
 
     }
 
