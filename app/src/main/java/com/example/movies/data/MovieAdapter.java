c6f3628297f8e83e14ed19e03cc455bf663d9623
Kholkin <kholkins@mail.ru>
2020-04-11 15:45:38 +0300

    model Movie

---
+++ app/src/main/java/com/example/movies/data/MovieAdapter.java
@@ -0,0 +1,56 @@
+package com.example.movies.data;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.movies.R;
+import com.example.movies.model.Movie;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.MovieViewHolder> {
+
+    private Context context;
+    private ArrayList<Movie> movies;
+
+    public MovieAdapter(Context context, ArrayList<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView posterImageView;
+        TextView titleTextView;
+        TextView yearTextView;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            posterImageView = itemView.findViewById(R.id.posterImageView);
+            titleTextView = itemView.findViewById(R.id.titleTextView);
+            yearTextView = itemView.findViewById(R.id.yearTextView);
+        }
+    }
+}
