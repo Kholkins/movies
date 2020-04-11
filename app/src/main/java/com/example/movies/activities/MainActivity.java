418de868d14ec8e9cd9542de6df04f6cdea78b1f
Kholkin <kholkins@mail.ru>
2020-04-11 16:56:30 +0300

    model Movie

--- app/src/main/java/com/example/movies/activities/MainActivity.java
+++ app/src/main/java/com/example/movies/activities/MainActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.movies.R;
 import com.example.movies.data.MovieAdapter;
 import com.example.movies.model.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -37,6 +46,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getMovies() {
-        String url = "http://www.omdbapi.com/?apikey=f6db0489&i=tt0073629";
+        String url = "http://www.omdbapi.com/?apikey=f6db0489&s=Rocky";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,
+                url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("Search");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
     }
 }
