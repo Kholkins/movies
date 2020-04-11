9f43e9bc081303519af584bfce6f7247ed5f32a2
Kholkin <kholkins@mail.ru>
2020-04-11 16:38:07 +0300

    model Movie

--- app/src/main/java/com/example/movies/activities/MainActivity.java
+++ app/src/main/java/com/example/movies/activities/MainActivity.java
@@ -1,16 +1,42 @@
 package com.example.movies.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.example.movies.R;
+import com.example.movies.data.MovieAdapter;
+import com.example.movies.model.Movie;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private ArrayList<Movie> movies;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        movies = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+
+        getMovies();
+    }
+
+    private void getMovies() {
+        String url = "http://www.omdbapi.com/?apikey=f6db0489&i=tt0073629";
     }
 }
